b8cd25721258014299a7692f0f459f02f1296b15
Gabriel Tobias <gabriel_tobias@outlook.com>
2024-08-14 05:45:10 -0300

    Deploy Secundario

---
+++ ecommerce/loja/novos_context.py
@@ -0,0 +1,19 @@
+from .models import Pedido, ItensPedido
+
+
+def carrinho(request):
+    quantidade_produtos_carrinho = 0
+    if request.user.is_authenticated:
+        cliente = request.user.cliente
+
+    else:
+        return {"quantidade_produtos_carrinho": quantidade_produtos_carrinho}
+    
+    pedido, criado = Pedido.objects.get_or_create(cliente=cliente, finalizado=False)
+
+    # Quantos produtos tem no pedido do usuário
+    itens_pedido = ItensPedido.objects.filter(pedido=pedido)
+    for item in itens_pedido:
+        quantidade_produtos_carrinho += item.quantidade
+        
+    return {"quantidade_produtos_carrinho": quantidade_produtos_carrinho}
---
+++ ecommerce/loja/views.py
@@ -0,0 +1,114 @@
+from django.shortcuts import render, redirect
+from .models import *
+
+# Create your views here.
+
+
+def homepage(request):
+    banners = Banner.objects.filter(ativo=True)
+    context = {"banners": banners}
+    return render(request, 'homepage.html', context)
+
+
+def loja(request, nome_categoria=None):
+    produtos = Produto.objects.filter(ativo=True)
+    if nome_categoria:
+        produtos = produtos.filter(categoria__nome=nome_categoria)
+    context = {"produtos": produtos}
+    return render(request, 'loja.html', context)
+
+
+def ver_produto(request, id_produto, id_cor=None):
+    tem_estoque = False
+    cores = {}
+    tamanhos = {}
+    cor_selecionada = None
+    if id_cor:
+        cor_selecionada = Cor.objects.get(id=id_cor)
+    produto = Produto.objects.get(id=id_produto)
+    itens_estoque = Itemestoque.objects.filter(produto=produto, quantidade__gt=0)
+    if len(itens_estoque) > 0:
+        tem_estoque = True
+        cores = {item.cor for item in itens_estoque}
+        if id_cor:
+            itens_estoque = Itemestoque.objects.filter(produto=produto, quantidade__gt=0, cor__id=id_cor)
+            tamanhos = {item.tamanho for item in itens_estoque}
+
+    context = {"produto": produto, "tem_estoque": tem_estoque,"cores": cores, "tamanhos": tamanhos, "cor_selecionada": cor_selecionada}
+
+    return render(request, 'ver_produto.html', context)
+
+
+def adicionar_carrinho(request, id_produto):
+    if request.method == 'POST' and id_produto:
+        dados = request.POST.dict()
+        tamanho = dados.get('tamanho')
+        id_cor = dados.get('cor')
+        if not tamanho:
+            return redirect('loja')
+        # Pegar o cliente
+        if request.user.is_authenticated:
+            cliente = request.user.cliente
+
+        else:
+            return redirect('loja')
+        pedido, criado  = Pedido.objects.get_or_create(cliente=cliente, finalizado=False)
+        item_estoque = Itemestoque.objects.get(produto__id = id_produto, tamanho = tamanho, cor__id = id_cor)
+        item_pedido, criado = ItensPedido.objects.get_or_create(item_estoque=item_estoque, pedido= pedido)
+        item_pedido.quantidade += 1
+        item_pedido.save()
+
+        # Criar um pedido ou pegar o pedido que esta em aberto
+        return redirect('carrinho')
+    else:
+        return redirect('loja')
+
+def remover_carrinho(request, id_produto):
+    if request.method == 'POST' and id_produto:
+        dados = request.POST.dict()
+        tamanho = dados.get('tamanho')
+        id_cor = dados.get('cor')
+        if not tamanho:
+            return redirect('loja')
+        # Pegar o cliente
+        if request.user.is_authenticated:
+            cliente = request.user.cliente
+
+        else:
+            return redirect('loja')
+        pedido, criado  = Pedido.objects.get_or_create(cliente=cliente, finalizado=False)
+        item_estoque = Itemestoque.objects.get(produto__id = id_produto, tamanho = tamanho, cor__id = id_cor)
+        item_pedido, criado = ItensPedido.objects.get_or_create(item_estoque=item_estoque, pedido= pedido)
+        item_pedido.quantidade -= 1
+        item_pedido.save()
+        if item_pedido.quantidade <= 0:
+            item_pedido.delete()
+
+    else:
+        return redirect('loja')
+    
+    return redirect('carrinho')
+
+
+def carrinho(request):
+    if request.user.is_authenticated:
+        cliente = request.user.cliente
+
+    pedido, criado = Pedido.objects.get_or_create(cliente=cliente, finalizado=False)
+    itens_pedido = ItensPedido.objects.filter(pedido=pedido)
+    context = {"itens_pedido":itens_pedido, "pedido":pedido}
+    return render(request, 'carrinho.html', context)
+
+
+def checkout(request):
+    return render(request, 'checkout.html')
+
+
+def minha_conta(request):
+    return render(request, 'usuario/minha_conta.html')
+
+
+def login(request):
+    return render(request, 'usuario/login.html')
+
+# TODO Sempre que um usuario criar uma conta no nosso site a gente cria um cliente para ele
